5333089e8c8f829598e39f055854c8892f5c627f
Oliver Galloway-Lunn <olivergl@akvo.org>
2013-10-24 17:21:14 +0100

    [#6] Updated content for Loïc's AkvoStaff plug-in and added the AkvoHeroBox and AkvoPartner plug-ins.

---
+++ akvo_org/wp-content/plugins/AkvoPartner/AkvoPartner.php
@@ -0,0 +1,144 @@
+<?php
+/*
+Plugin Name: Akvo partner admin
+Plugin URI: http://akvo.org/about-us/partners/
+Description: Add and manage new Akvo partner easily.
+Version: 1.0
+Author: Loic Sans
+Author URI: http://akvo.org/
+*/
+
+
+add_action( 'init', 'create_new_partner' );
+
+function create_new_partner() {
+    register_post_type( 'new_partners',
+        array(
+            'labels' => array(
+                'name' => 'Akvo partner',
+                'singular_name' => 'New partner',
+                'add_new' => 'Add a new partner',
+                'add_new_item' => 'Add new partner',
+                'edit' => 'Edit',
+                'edit_item' => 'Edit partner',
+                'new_item' => 'New partner',
+                'view' => 'View',
+                'view_item' => 'View partner',
+                'search_items' => 'Search partner',
+                'not_found' => 'No partner found',
+                'not_found_in_trash' => 'No partner found in Trash',
+                'parent' => 'Parent partner'
+            ),
+ 
+            'public' => true,
+            'menu_position' => 14,
+            'supports' => array( 'title', 'editor','author', 'thumbnail', 'revisions'),
+            'taxonomies' => array( '' ),
+            'menu_icon' => plugins_url( 'images/akvoPartner_icn.png', __FILE__ ),
+            'has_archive' => true
+        )
+    );
+}
+add_theme_support( 'post-thumbnails' );
+set_post_thumbnail_size( 240, 135, true );
+add_action( 'admin_init', 'partner_admin' );
+
+function partner_admin() {
+    add_meta_box( 'new_partner_meta_box',
+        'New partner details',
+        'display_new_partner_meta_box',
+        'new_partners', 'normal', 'high'
+    );
+}
+
+function display_new_partner_meta_box( $new_partner ) {
+    // Retrieve current name of the partner and title based on partner ID
+    $partner_name = esc_html( get_post_meta( $new_partner->ID, 'partner_name', true ) );
+    $partner_tagline = esc_html( get_post_meta( $new_partner->ID, 'partner_tagline', true ) );
+    $partner_descr = esc_html( get_post_meta( $new_partner->ID, 'partner_descr', true ) );
+    ?>
+    <table>
+        <tr>
+            <td style="width: 100%">Partner name</td>
+            <td><input type="text" size="80" name="new_partner_name" value="<?php echo $partner_name; ?>" /></td>
+        </tr>
+        <tr>
+            <td style="width: 100%">Partner tagline</td>
+            <td><input type="text" size="80" name="new_partner_tagline" value="<?php echo $partner_tagline; ?>" /></td>
+        </tr>
+        <tr>
+            <td style="width: 100%">Partner Description</td>
+            <td><textarea type="text" size="80" name="new_partner_descr" value="<?php echo $partner_descr; ?>"></textarea></td>
+        </tr>
+    </table>
+    <?php
+}
+
+add_action( 'save_post',
+'add_new_partner_fields', 10, 2 );
+
+function add_new_partner_fields( $new_partner_id, $new_partner ) {
+    // Check post type for new partners
+    if ( $new_partner->post_type == 'new_partners' ) {
+        // Store data in post meta table if present in post data
+        if ( isset( $_POST['new_partner_name'] ) && $_POST['new_partner_name'] != '' ) {
+            update_post_meta( $new_partner_id, 'partner_name', $_POST['new_partner_name'] );
+        }
+        if ( isset( $_POST['new_partner_tagline'] ) && $_POST['new_partner_tagline'] != '' ) {
+            update_post_meta( $new_partner_id, 'partner_tagline', $_POST['new_partner_tagline'] );
+        }
+        if ( isset( $_POST['new_partner_descr'] ) && $_POST['new_partner_descr'] != '' ) {
+            update_post_meta( $new_partner_id, 'partner_descr', $_POST['new_partner_descr'] );
+        }
+    }
+}
+
+
+add_filter( 'template_include', 'include_partnertemplate_function', 1 );
+
+function include_partnertemplate_function( $template_path ) {
+    if ( get_post_type() == 'new_partners' ) {
+        if ( is_single() ) {
+            // checks if the file exists in the theme first,
+            // otherwise serve the file from the plugin
+            if ( $theme_file = locate_template( array ( 'single-new_partners.php' ) ) ) {
+                $template_path = $theme_file;
+            } else {
+                $template_path = plugin_dir_path( __FILE__ ) . '/single-new_partners.php';
+            }
+        }
+		  elseif ( is_archive() ) {
+            if ( $theme_file = locate_template( array ( 'archive-new_partners.php' ) ) ) {
+                $template_path = $theme_file;
+            } else { $template_path = plugin_dir_path( __FILE__ ) . '/archive-new_partners.php';
+ 
+            }
+    }
+	}
+    return $template_path;
+}
+
+//CREATE CUSTOM TAXONOMIES
+
+add_action( 'init', 'create_partner_taxonomies', 0 );
+
+function create_partner_taxonomies() {
+
+    register_taxonomy(
+        'new_partners_category',
+        'new_partners',
+        array(
+            'labels' => array(
+                'name' => 'Akvo partner category',
+                'add_new_item' => 'Add new Akvo category',
+                'new_item_name' => "New Akvo category"
+            ),
+            'show_ui' => true,
+            'show_tagcloud' => false,
+            'hierarchical' => true,
+			 'query_var' => true,
+			'rewrite' => array('slug' => 'new_partners_category' )
+        )
+    );
+}
+?>
--- akvo_org/wp-content/plugins/AkvoStaff/AkvoStaff.php
+++ akvo_org/wp-content/plugins/AkvoStaff/AkvoStaff.php
@@ -1,11 +1,11 @@
 <?php
 /*
-Plugin Name: Akvo staff
-Plugin URI: http://akvo.org
-Description: Add new Akvo team member easily.
+Plugin Name: Akvo staff admin
+Plugin URI: http://akvo.org/about-us/team/
+Description: Add new Akvo team member and manage them easily.
 Version: 1.0
 Author: Loic Sans
-Author URI: http://loicsans.com/
+Author URI: http://akvo.org/
 */
 
 
@@ -15,24 +15,24 @@ function create_new_staff() {
     register_post_type( 'new_staffs',
         array(
             'labels' => array(
-                'name' => 'Akvo Staff',
-                'singular_name' => 'New Staff',
-                'add_new' => 'Add New',
-                'add_new_item' => 'Add New New Staff',
+                'name' => 'Akvo staff',
+                'singular_name' => 'New staff',
+                'add_new' => 'Add a new staff',
+                'add_new_item' => 'Add new staff',
                 'edit' => 'Edit',
-                'edit_item' => 'Edit New Staff',
-                'new_item' => 'New New Staff',
+                'edit_item' => 'Edit staff',
+                'new_item' => 'New staff',
                 'view' => 'View',
-                'view_item' => 'View New Staff',
-                'search_items' => 'Search New Staffs',
-                'not_found' => 'No New Staffs found',
-                'not_found_in_trash' => 'No New Staffs found in Trash',
-                'parent' => 'Parent New Staff'
+                'view_item' => 'View staff',
+                'search_items' => 'Search staff',
+                'not_found' => 'No staff found',
+                'not_found_in_trash' => 'No staff found in Trash',
+                'parent' => 'Parent staff'
             ),
  
             'public' => true,
             'menu_position' => 15,
-            'supports' => array( 'title', 'editor', 'thumbnail', 'custom-fields' ),
+            'supports' => array( 'title', 'editor', 'author','thumbnail', 'revisions'),
             'taxonomies' => array( '' ),
             'menu_icon' => plugins_url( 'images/akvoStaff_icn.png', __FILE__ ),
             'has_archive' => true
@@ -59,15 +59,15 @@ function display_new_staff_meta_box( $new_staff ) {
     ?>
     <table>
         <tr>
-            <td style="width: 100%">New Staff Name</td>
+            <td style="width: 100%">Full Name</td>
             <td><input type="text" size="80" name="new_staff_name" value="<?php echo $staff_name; ?>" /></td>
         </tr>
         <tr>
-            <td style="width: 100%">New Staff title</td>
+            <td style="width: 100%">Job title</td>
             <td><input type="text" size="80" name="new_staff_title" value="<?php echo $staff_title; ?>" /></td>
         </tr>
-        <tr>
-            <td style="width: 100%">New Staff DEscription</td>
+      <tr>
+            <td style="width: 100%">Biography</td>
             <td><textarea type="text" size="80" name="new_staff_descr" value="<?php echo $staff_descr; ?>"></textarea></td>
         </tr>
     </table>
@@ -123,18 +123,21 @@ function include_template_function( $template_path ) {
 add_action( 'init', 'create_my_taxonomies', 0 );
 
 function create_my_taxonomies() {
+
     register_taxonomy(
         'new_staffs_team',
         'new_staffs',
         array(
             'labels' => array(
                 'name' => 'Akvo staff team',
-                'add_new_item' => 'Add New Akvo Team',
-                'new_item_name' => "New Akvo Team"
+                'add_new_item' => 'Add new Akvo team',
+                'new_item_name' => "New Akvo team"
             ),
             'show_ui' => true,
             'show_tagcloud' => false,
-            'hierarchical' => true
+            'hierarchical' => true,
+			 'query_var' => true,
+			'rewrite' => array('slug' => 'new_staffs_team' )
         )
     );
 }
