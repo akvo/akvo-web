e7989bdd53ec0e5b7cf12fca96fae90908b860fa
Loic <loic.sans@gmail.com>
2014-04-29 13:02:45 +0300

    Fixes RSS link on th eall updates page

--- code/wp-content/themes/Akvo-responsive/rsrUpdatesPage.php
+++ code/wp-content/themes/Akvo-responsive/rsrUpdatesPage.php
@@ -9,7 +9,7 @@
 
 <div id="content" class="floats-in networkPage">
   <h1 class="backLined">All latest project updates</h1>
-    <nav class="anchorNav wrapper"><ul><li  class="rss"><a href="http://rsr.akvo.org/rss/all-updates/">RSS Link for All RSR Updates ></a></li></ul></nav>
+    <nav class="anchorNav2 wrapper"><ul><li  class="rss"><a href="http://rsr.akvo.org/rss/all-updates/">RSS Link for All RSR Updates ></a></li></ul></nav>
   <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
   <div class="fullWidthParag wrapper"><?php the_content(); ?></div>
 
