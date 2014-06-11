924e3ed4d8284b1fe1113f8024f50c2fc7ff368f
Oliver Galloway-Lunn <olivergl@akvo.org>
2014-06-11 10:56:13 +0100

    [#265] Corrected Openaid JSON data templates.

--- code/wp-content/themes/Akvo-responsive/json_data_templates/openaid-org.template.php
+++ code/wp-content/themes/Akvo-responsive/json_data_templates/openaid-org.template.php
@@ -1,10 +1,10 @@
 <?php
 /*
-* Template for the "OpenAid activities" plugin used on networkPage.php
-* Feed name: OpenAid activities
-* Slug: openaid-activities
-* JSON feed URL: http://oipa.openaidsearch.org/api/v2/activities/?format=json&limit=1
-* Resulting shortcode: [jsondata_feed slug="openaid-activities" format="json" limit="1"]
+* Template for the "OpenAid orgs" plugin used on networkPage.php
+* Feed name: OpenAid orgs
+* Slug: openaid-orgs
+* JSON feed URL: http://oipa.openaidsearch.org/api/v2/organisations/?format=json&limit=1
+* Resulting shortcode: [jsondata_feed slug="openaid-orgs" format="json" limit="1"]
 */
   echo $aData['meta']['total_count'];
 ?>
