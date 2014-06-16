99b88caa81335355dcd7fd6aff2bfc1981290c3b
Gabriel von Heijne <gabriel@akvo.org>
2014-06-16 12:23:01 +0200

    [#291] Incremental loading of updates
    Load more updates using jQuery on /seeithappen/all-rsr-project-updates/
    
    This fix also requires the setting of two WP Custom fields,
    starting_number_of_updates and number_of_updates_to_add.

---
+++ code/wp-content/themes/Akvo-responsive/moreUpdates.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Template Name: moreUpdates
+ */
+//phpinfo();
+  $limit = $_REQUEST['limit'];
+  $offset = $_REQUEST['offset'];
+  $shortcode = '[jsondata_feed slug="rsr-many-updates" photo__gte="a" limit="'. $limit .'" offset="'. $offset .'"]';
+  do_shortcode($shortcode);
+?>
