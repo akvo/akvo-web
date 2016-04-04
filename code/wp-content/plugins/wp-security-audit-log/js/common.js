3d60e51f2d17174f318772ab8c3d05246329de53
Lynn Greenwood <lynn@akvo.org>
2016-04-04 12:18:25 +0200

    [#900] Security Activity Log

---
+++ code/wp-content/plugins/wp-security-audit-log/js/common.js
@@ -0,0 +1,17 @@
+
+jQuery(document).ready(function(){
+	jQuery('a.wsal-dismiss-notification').click(function(){
+		var nfe = jQuery(this).parents('div:first');
+		var nfn = nfe.attr('data-notice-name');
+		jQuery.ajax({
+			type: 'POST',
+			url: ajaxurl,
+			async: false,
+			data: { action: 'AjaxDismissNotice', notice: nfn }
+		});
+		nfe.fadeOut();
+	});
+	
+    jQuery('head').append('<style>.wp-submenu .dashicons-external:before{vertical-align: bottom;}</style>');
+	jQuery("a[href*='page=wsal-extensions']").addClass('dashicons-before dashicons-external').css('color', '#CC4444');
+});
