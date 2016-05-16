f4dae08169d39f733b5f85fd085f260ab0027e79
Samuel Thomas <samvthom16@gmail.com>
2016-05-17 01:08:53 +0530

    [#944] updated the author in meta

--- code/wp-content/themes/Akvo-responsive/header.php
+++ code/wp-content/themes/Akvo-responsive/header.php
@@ -25,7 +25,7 @@
 <meta name="description" content="Akvo.org: See it Happen" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<meta name="author" content="Loic Sans">
+<meta name="author" content="Akvo Foundation">
 <meta name="HandheldFriendly" content="True">
 <meta name="google-site-verification" content="h6M7-buFJgu3jUVcALgFkBDxi0UtePXQhVUcZBYO5Lk" />
 <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
