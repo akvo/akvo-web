ec17054c0bf93c6cdb58824c77c8bd448a9873d1
Samuel Thomas <samvthom16@gmail.com>
2017-07-27 09:39:43 +0530

    [#1180] - fix akvo sites carousel

--- code/wp-content/themes/Akvo-responsive/productpage-sites.php
+++ code/wp-content/themes/Akvo-responsive/productpage-sites.php
@@ -13,9 +13,9 @@
 			'title' => 'overview',
 			'tagline' => 'overview_tagline',
 			'elements' => array(
-				'overview_cover' => 'akvo_page_section',
-				'overview_intro' => 'akvo_tab_inner_section',
-				'overview_buttons' => 'akvo_tab_buttons',
+				'overview_cover' => 'carousel',
+				'overview_intro' => 'inner_section',
+				'overview_buttons' => 'buttons',
 				'testimonials' => 'testimonials'
 			)
 		),
@@ -23,7 +23,7 @@
 			'title' => 'features',
 			'tagline' => 'features_tagline',
 			'elements' => array(
-				'features_cover' => 'akvo_page_section',
+				'features_cover' => 'page_section',
 				'features_columns' => 'features_columns'
 			)
 		),
@@ -31,17 +31,17 @@
 			'title' => 'pricing',
 			'tagline' => 'pricing_tagline',
 			'elements' => array(
-				'pricing_cover' => 'akvo_page_section',
-				'pricing_intro' => 'akvo_tab_inner_section',
-				'pricing_buttons' => 'akvo_tab_buttons',
-				'pricing_ending' => 'akvo_tab_inner_section'
+				'pricing_cover' => 'page_section',
+				'pricing_intro' => 'inner_section',
+				'pricing_buttons' => 'buttons',
+				'pricing_ending' => 'inner_section'
 			)
 		),
 		'gallery' => array(
 			'title' => 'gallery',
 			'tagline' => 'gallery_tagline',
 			'elements' => array(
-				'gallery_cover' => 'akvo_page_section',
+				'gallery_cover' => 'page_section',
 				'section' => 'gallery'
 				
 			)
@@ -51,58 +51,83 @@
 	
 	
 	
-	
-	
-	
-	function features_columns($el){
-	?>
-		<div id="<?php _e($el);?>" class='sub-section'>
-		<div class='threeColumns wrapper'>
-		<?php while(have_rows($el)): the_row();?>
-			<div>
-				<?php
-					$boxes = get_sub_field('columns');
-					foreach($boxes as $box):
-				?>
-				<div class='box-col'>
-					<i class="fa fa-2x <?php _e($box['icon']);?>"></i>
-					<p><?php _e($box['description']);?></p>
-				</div>	
-				<?php endforeach;?>
-			<p><?php _e(get_sub_field('description'));?></p>
+	class sitesTab extends akvoTab{
+		
+		function features_columns($el){
+		?>
+			<div id="<?php _e($el);?>" class='sub-section'>
+			<div class='threeColumns wrapper'>
+			<?php while(have_rows($el)): the_row();?>
+				<div>
+					<?php
+						$boxes = get_sub_field('columns');
+						foreach($boxes as $box):
+					?>
+					<div class='box-col'>
+						<i class="fa fa-2x <?php _e($box['icon']);?>"></i>
+						<p><?php _e($box['description']);?></p>
+					</div>	
+					<?php endforeach;?>
+				<p><?php _e(get_sub_field('description'));?></p>
+				</div>
+			<?php endwhile;?>
 			</div>
-		<?php endwhile;?>
-		</div>
-		<div class='clearfix'></div>
-		</div>
-	<?php }
+			<div class='clearfix'></div>
+			</div>
+		<?php }
+		
+		
+		function gallery($el){
+		?> 
+		<div class="sub-section" id="<?php _e($el);?>">
+			<div class="row">
+				<?php while( have_rows($el) ): the_row(); ?>
+				<div class='col-6'>
+					<a href="<?php the_sub_field('image_link'); ?>">
+						<img src="<?php the_sub_field('image'); ?>" title="akvosites"/>
+						<?php the_sub_field('description');?>
+					</a>
+				</div>
+				<?php endwhile; ?>
+			</div>
+		</div>	
+		<?php
+		}
+		
+		
+	}
 	
 	
-	function gallery($el){
-	?> 
-	<div class="sub-section" id="<?php _e($el);?>">
-		<div class="row">
-    		<?php while( have_rows($el) ): the_row(); ?>
-      		<div class='col-6'>
-      			<a href="<?php the_sub_field('image_link'); ?>">
-      				<img src="<?php the_sub_field('image'); ?>" title="akvosites"/>
-      				<?php the_sub_field('description');?>
-      			</a>
-      		</div>
-    		<?php endwhile; ?>
-    	</div>
-    </div>	
-    <?php
-    }
+	
 ?>
 
 <div id="content" class="floats-in productPage akvositesProduct">
-	<?php akvo_tabs($tabs);?>
+	<?php
+		$akvo_tab = new sitesTab;
+		$akvo_tab->display_tabs($tabs);
+	?>
 </div>  
 <?php get_footer(); ?>
 <script type="text/javascript" src="<?php bloginfo('template_url');?>/js/tabs.js"></script>
 <script type="text/javascript">
 	(function($){
-		$('body').find('[data-behaviour~=akvo-tabs]').akvo_tabs();
-	}(jQuery));  
-</script>	
+		
+		$('.bxslider').bxSlider({
+  			onSliderLoad: function(){
+  				console.log('slider:after-load');
+    			
+    			$('body').find('[data-behaviour~=akvo-tabs]').akvo_tabs();
+    			
+    			$('[data-behaviour~=show-video]').click( function(event) {
+      				event.preventDefault();
+      				$('.videoContainer').fadeToggle();
+    			});
+    			
+    			
+    			$('[data-behaviour~=time-ticker]').rsr_time_ticker();
+    			$('[data-behaviour~=anchor-reload]').rsr_anchor_reload();
+    			
+    		}
+		});
+    }(jQuery));  
+</script>
