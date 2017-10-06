924251d53595d43fcef6b843b421abfc7609cc93
Samuel Thomas <samvthom16@gmail.com>
2017-10-06 09:06:59 +0530

    [#1203] - fix duplicate title

--- code/wp-content/themes/Akvo-responsive/header.php
+++ code/wp-content/themes/Akvo-responsive/header.php
@@ -13,7 +13,7 @@
 			<?php
 				global $page, $paged;
 				wp_title( '|', true, 'right' );
-				bloginfo( 'name' );
+				//bloginfo( 'name' );
 				$site_description = get_bloginfo( 'description', 'display' );
 				if ( $site_description && ( is_home() || is_front_page() ) )
 					echo " | $site_description";
