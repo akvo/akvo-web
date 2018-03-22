6cff03f35aa6403f453546b2e86f8f7dbfbc0ebd
Samuel Thomas <samvthom16@gmail.com>
2018-03-22 13:21:42 +0530

    [#1270, #1263] - regional page header content to be flexible

--- code/wp-content/themes/Akvo-responsive/searchform.php
+++ code/wp-content/themes/Akvo-responsive/searchform.php
@@ -5,7 +5,7 @@
  * @subpackage akvo.org
  */
 ?>
-	<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-		<input type="search" class="field" name="s" id="s" placeholder="<?php esc_attr_e( 'Search', 'akvo.org' ); ?>" />
-		<input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'akvo.org' ); ?>" />
-	</form>
+<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<input type="search" class="field" name="s" id="s" placeholder="<?php esc_attr_e( 'Search', 'akvo.org' ); ?>" />
+	<input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'akvo.org' ); ?>" />
+</form>
