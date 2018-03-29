5d328974132a3f1c3101db77b3854515fe222a12
Samuel Thomas <samvthom16@gmail.com>
2018-03-29 09:45:45 +0530

    [#1225] - search bar icon

--- code/wp-content/themes/Akvo-responsive/searchform.php
+++ code/wp-content/themes/Akvo-responsive/searchform.php
@@ -6,6 +6,6 @@
  */
 ?>
 <form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<input type="search" class="field" name="s" id="s" placeholder="<?php esc_attr_e( 'Search', 'akvo.org' ); ?>" />
+	<input type="search" class="field" name="s" id="s" placeholder="<?php if( !is_akvo_full_black_body() ){ esc_attr_e( 'Search', 'akvo.org' ); } ?>" />
 	<input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'akvo.org' ); ?>" />
 </form>
