3bed203d3b462ee300356443900b344202047172
Samuel Thomas <samvthom16@gmail.com>
2018-04-04 14:54:39 +0530

    [#1280] - new page builder template for the new layout

---
+++ code/wp-content/themes/Akvo-responsive/fullBlackLayout.php
@@ -0,0 +1,11 @@
+<?php
+/*
+	Template Name: Fullblack Layout
+*/
+?>
+<?php get_header();?>
+<div id="content" class="floats-in">
+	<?php if ( have_posts() ): while ( have_posts() ) : the_post(); the_content(); endwhile; endif; ?>
+	<?php the_hubs_list('Find us in five continents');?>	
+</div>
+<?php get_footer();?>
