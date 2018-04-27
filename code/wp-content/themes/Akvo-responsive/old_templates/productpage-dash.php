b3d8a608807ae29eb56041786b089f4036745916
Samuel Thomas <samvthom16@gmail.com>
2018-04-27 09:36:56 +0530

    [#1082] - copy of old templates in a seperate folder

---
+++ code/wp-content/themes/Akvo-responsive/old_templates/productpage-dash.php
@@ -0,0 +1,84 @@
+<?php
+  /*
+    Template Name: product-dash-v1.0
+  */
+?>
+<?php get_header(); ?>
+<!--Start of Akvo.org DASH Product Page-->
+
+<div id="content" class="floats-in productPage withSubMenu dashProduct">
+	
+	
+  	<hgroup>
+  		<img class='prod-logo' src="<?php the_field('logo');?>" />
+    	<!--h1><?php the_field('rsr_name'); ?></h1-->
+    	<h2 id="tagline"><?php the_field('tagline');?></h2>
+    </hgroup>
+	
+	
+	<?php 
+		$row_i = 0;
+		while(have_rows('section')): 
+			the_row();
+			$image_flag = false;
+			
+			$image_text = get_sub_field('image_text');
+			
+			if (strpos($image_text, '<img') !== false) {
+ 			   $image_flag = true;
+			}
+	?>
+  	<section>
+  		<div class="full-width-banner <?php if($row_i){_e('shallow-banner');}?> <?php if($image_flag){_e('overlay-banner');}?>" style="background-image:url(<?php _e(get_sub_field('image'));?>);">
+        	<?php if($image_text):?>
+           	<a href="<?php _e(get_sub_field('image_link'));?>">
+           		<?php _e($image_text);?>
+           	</a>
+           	<?php endif;?>
+    	</div>
+    	<?php $desc = get_sub_field('description');if($desc):?>
+    	<div class='page-section'><?php _e($desc);?></div>
+    	<?php endif;?>
+  	</section>
+  	<?php $row_i++;endwhile;?>
+  	
+  	<section>
+  		<div class='page-section full-width light-green-bg'>
+  			<?php the_field('components_section');?>
+  			<br>
+  			<ul>
+  				<?php while(have_rows('components')): the_row();?>
+  				<li class="box">
+  					<i class="fa fa-2x <?php the_sub_field('icon');?>"></i>
+  					<h4><?php the_sub_field('title');?></h4>
+  					<p><?php the_sub_field('description');?></p>
+  				</li>
+  				<?php endwhile;?>
+  				<li class="clearfix"></li>
+  			</ul>
+  		</div>
+  	</section>
+  	
+  	<section>
+  		<div class='page-section'>
+  			<?php the_field('get_involved_section');?>
+  			<ul class="text-center">
+  				<?php while(have_rows('get_involved_buttons')): the_row();?>
+  				<li><a class="button green-bg" href="<?php the_sub_field('link');?>"><?php the_sub_field('text');?></a></li>
+  				<?php endwhile;?>
+  			</ul>
+  		</div>	
+  	</section>
+  	
+  	<section class="grey-bg">
+  		<div class='page-section'>
+  			<?php the_field('about_section');?>
+  		</div>
+  	</section>
+  	
+</div>  
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+
+<?php get_footer(); ?>
---
+++ code/wp-content/themes/Akvo-responsive/old_templates/productpage-rsr-new.php
@@ -0,0 +1,285 @@
+<?php
+  /*
+    Template Name: product-rsr-v3.1
+  */
+?>
+<?php get_header(); ?>
+<!--Start of Akvo.org RSR Product Page-->
+
+<?php
+	$tabs = array(
+		'overview' => array(
+			'title' => 'overview',
+			'tagline' => 'overview_tagline',
+			'elements' => array(
+				'overview_carousel' 		=> 'carousel',
+				'overview_columns' 			=> 'overview_columns3',
+				'overview_call_to_action' 	=> 'rsr_overview_buttons',
+				'testimonials' 				=> 'testimonials',
+				'counters'					=> 'rsr_overview_counters',
+				'overview_buttons' 			=> 'buttons'
+			)
+		),
+		'features' => array(
+			'title' => 'features',
+			'tagline' => 'feature_tagline',
+			'elements' => array(
+				'feature_banner' 	=> 'rsr_banner',
+				'feature_columns' 	=> 'rsr_feature_columns',
+				'tour' 				=> 'rsr_tour',
+				'features_section' 	=> 'rsr_features_section'
+				
+			)
+		),
+		'support' => array(
+			'title' => 'support',
+			'tagline' => 'support_tagline',
+			'elements' => array(
+				'support_banner' 			=> 'rsr_banner',
+				'support_content' 			=> 'inner_section',
+				'support_training_title' 	=> 'rsr_title',
+				'support_columns' 			=> 'overview_columns3',
+				'support_button_title' 		=> 'inner_section',
+				'support_buttons' 			=> 'buttons',
+				'support_testimonial_title' => 'rsr_title',
+				'support_testimonials' 		=> 'testimonials'
+				
+			)
+		),
+		'pricing' => array(
+			'title' => 'pricing',
+			'tagline' => 'pricing_tagline',
+			'elements' => array(
+				'pricing_banner' 		=> 'rsr_banner',
+				'pricing_image' 		=> 'image',
+				'pricing_description' 	=> 'inner_section',
+				'pricing_buttons' 		=> 'buttons',
+				'pricing_description_2' => 'inner_section'
+			)
+		)
+		
+	);
+	
+	
+	class rsrTab extends akvoTab{
+	
+		function rsr_title($el){
+			_e("<h3 id=".$el.">".get_field($el)."</h3>");
+		}
+		
+		/*
+		function rsr_overview_columns($el){
+			_e("<div class='sub-section' id='".$el."'>");
+			_e("<div class='threeColumns wrapper'>");
+			while(have_rows($el)): the_row();?>
+				<div>
+					<img src="<?php the_sub_field('icon');?>" />
+					<h3><?php the_sub_field('title');?></h3>
+					<p><?php the_sub_field('description');?></p>
+				</div>
+				
+			<?php endwhile;
+			_e("</div><div class='clearfix'></div>");
+			_e("</div>");
+		}
+		*/
+		
+		function rsr_overview_buttons($el){
+		?>
+			<div class='sub-section'>
+				<ul class="text-center list-inline">
+					<li>
+						<a data-behaviour="show-video" href="#video" title="<?php the_field('video_link_text'); ?>" class="button"><?php the_field('video_link_text'); ?></a>
+					</li>
+					<li>
+						<a href="<?php the_field('tour_link'); ?>" data-behaviour="anchor-reload" title="<?php the_field('tour_link_text'); ?>" class="button"><?php the_field('tour_link_text'); ?></a>
+					</li>
+				</ul>
+				<div id="video" class="videoContainer wrapper">
+					<div class="vimeoBlockedMessage">
+						<?php the_field('rsr_video_backup_message'); ?>
+					</div>
+					<iframe width="600" height="300" frameborder="0" allowfullscreen="" mozallowfullscreen="" webkitallowfullscreen="" src="<?php the_field('video_link'); ?>"></iframe>
+				</div>
+			</div>	
+			
+		<?php	
+		}
+		
+		
+		function rsr_overview_counters($el){
+			echo "<div class='sub-section' data-behaviour='time-ticker'>";
+			
+			$json = akvo_json('http://rsr.akvo.org/api/v1/right_now_in_akvo/?format=json');
+			
+			?>
+			<ul class="list-box">
+				<li class="box">
+					<a href="http://rsr.akvo.org/en/organisations/">      		
+						<h4>Organisations<br></h4>
+						<div class="timeTicker">            
+							<p class="timeSegment clear"><?php $this->counter_css($json->number_of_organisations);?></p>
+						</div>
+					</a>
+				</li>
+				<li class="box">
+					<a href="http://rsr.akvo.org/en/projects/">
+						<h4>Projects<br></h4>
+						<div class="timeTicker">            
+							<p class="timeSegment clear"><?php $this->counter_css($json->number_of_projects);?></p>
+						</div>
+					</a>
+				</li>
+			</ul>
+				<?php
+			
+			echo "</div>";
+		}
+		
+		function rsr_feature_columns($el){
+		?>
+			<div class='sub-section'>
+			<div class='threeColumns wrapper'>
+			<?php while(have_rows($el)): the_row();?>
+				<div>
+					<img src="<?php the_sub_field('icon');?>" />
+					<h3><?php _e(get_sub_field('heading'));?></h3>
+					<?php
+						$boxes = get_sub_field('box');
+						foreach($boxes as $box):
+					?>
+					<div class="box-col">
+						<i class="fa fa-2x <?php _e($box['icon']);?>"></i>
+						<h4><?php _e($box['title']);?></h4>
+						<p><?php _e($box['description']);?></p>
+					</div>	
+					<?php endforeach;?>
+				<p><?php _e(get_sub_field('description'));?></p>
+				</div>
+			<?php endwhile;?>
+			</div>
+			<div class='clearfix'></div>
+			</div>
+		<?php }
+		
+		
+		
+		function rsr_tour(){
+			_e("<div class='sub-section'>");
+			_e("<h3>".get_field('tour_title')."</h3>");
+			$i = 0;
+			while(have_rows('tour')): the_row();$i++;?>
+				<div class="screenshot" id="screenshot-<?php _e($i);?>">
+					<h4><?php the_sub_field('title');?></h4>
+					<p><?php the_sub_field('description');?></p>
+					<img src="<?php the_sub_field('image');?>" />
+				</div>
+			<?php endwhile;
+			_e("</div>");
+		}
+		
+		function rsr_banner($el){
+		?>
+			<div class="banner" style="background-image:url('<?php the_field($el);?>');"></div>
+		<?php	
+		}
+		/*
+		function rsr_buttons($el){
+			
+		?>
+			<div class='sub-section' id="<?php _e($el);?>">
+				<ul class='text-center list-inline'>
+					<?php while(have_rows($el)): the_row();
+						$desc = get_sub_field('description');
+					?>
+					<li>
+						<?php if($desc):?><h4><?php _e($desc);?></h4><br><?php endif;?>
+						<a href="<?php the_sub_field('link');?>" title="<?php the_sub_field('text'); ?>" class="button"><?php the_sub_field('text'); ?></a>
+					</li>
+					<?php endwhile;?>
+				</ul>
+			</div>	
+			
+			
+		<?php
+			
+		}
+		*/
+		
+		function rsr_media_section($el){
+		
+		?>
+			<div class="sub-section">
+				<div class="wrapper">
+					
+					<?php $sections = get_field($el);?>
+					
+					<ul>
+						<?php foreach($sections as $section):?>
+						<li class="media-box">
+							
+								<div class="media-big <?php if($section['image_text']):?>media-left<?php else:?>media-right<?php endif;?>">
+									<h3><?php _e($section['title']);?></h3>
+									<p><?php _e($section['description']); ?></p>
+								</div>
+								<div class="media-small text-center <?php if($section['image_text']):?>media-right<?php else:?>media-left<?php endif;?>">
+									<a href="<?php _e($section['link']);?>"><img src="<?php _e($section['image']); ?>" /></a>	
+									<?php _e($section['image_text']);?>
+								</div>
+							
+							<div class="clear"></div>
+						</li>
+						<?php endforeach;?>
+					</ul>
+					
+					
+					
+				</div>
+			</div>
+			
+	  <?php
+		
+		}
+		
+		function rsr_features_section($el){
+		
+		?>
+			<div class="sub-section" id="<?php _e($el);?>">
+				<div class="wrapper">
+					
+					<?php $sections = get_field($el);?>
+					<ul>
+						<?php foreach($sections as $section):?>
+						<li class="media-box">
+								<h3><?php _e($section['title']);?></h3>
+								<div class="media-big media-left">
+									<p><?php _e($section['description']); ?></p>
+								</div>
+								<div class="media-small media-right">
+									<a href="<?php _e($section['link']);?>"><img src="<?php _e($section['image']); ?>" /></a>	
+									<?php _e($section['image_text']);?>
+								</div>
+							
+							<div class="clear"></div>
+						</li>
+						<?php endforeach;?>
+					</ul>
+					
+					
+					
+				</div>
+			</div>
+			
+	  <?php
+		
+		}
+	}
+?>
+
+<div id="content" class="floats-in productPage withSubMenu rsrProdPag" data-behaviour="tabs-page">
+	<?php
+		$akvo_tab = new rsrTab;
+		$akvo_tab->display_tabs($tabs);
+	?>
+</div>  
+<?php get_footer(); ?>
