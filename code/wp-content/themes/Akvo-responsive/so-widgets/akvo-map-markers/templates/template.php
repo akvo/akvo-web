354b2e707a0094a0b603a76be101d936419e49c8
Samuel Thomas <samvthom16@gmail.com>
2018-07-05 10:29:41 +0530

    [#1291] - move styles of map markers to css

--- code/wp-content/themes/Akvo-responsive/so-widgets/akvo-map-markers/templates/template.php
+++ code/wp-content/themes/Akvo-responsive/so-widgets/akvo-map-markers/templates/template.php
@@ -5,9 +5,9 @@
 	$map_url = $akvo_widgets_template->get_image_url( $instance['map'] );
 	
 ?>
-<div style='position:relative;background-image:url("<?php echo $map_url;?>");padding-bottom:49%;width:100%;background-repeat:no-repeat;background-position:center;background-size:91%;'>
+<div class="map-markers" style='background-image:url("<?php echo $map_url;?>");'>
 	<?php foreach( $instance['markers'] as $marker ): $img_src = $akvo_widgets_template->get_image_url( $marker['image'] );?>
-	<a target="_blank" href="<?php echo $marker['link'];?>" style='position:absolute; left:<?php echo $marker['left'];?>%;top:<?php echo $marker['top'];?>%;'>
+	<a target="_blank" href="<?php echo $marker['link'];?>" style='left:<?php echo $marker['left'];?>%;top:<?php echo $marker['top'];?>%;'>
 		<img src="<?php echo $img_src;?>" />
 	</a>
 	<?php endforeach;?>
