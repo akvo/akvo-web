ddc68737ba1f282ec6bd734c5166774dd56c067e
Samuel Thomas <samvthom16@gmail.com>
2018-07-12 16:19:28 +0530

    [#1301] - enable cache through so widget

--- code/wp-content/themes/Akvo-responsive/inc/shortcodes/class-akvo-shortcode.php
+++ code/wp-content/themes/Akvo-responsive/inc/shortcodes/class-akvo-shortcode.php
@@ -86,4 +86,27 @@
 			
 		}
 		
+		/* PASS AN ARRAY TO CREATE ATTRIBUTES OF SHORTCODE */
+		function form_shortcode( $data ){
+			
+        	$default_atts = $this->get_default_atts(); 				// GET DEFAULT ATTS OF THE SHORTCODE 
+			
+			$shortcode = '['.$this->shortcode.' ';					// SHORTCODE STRING START 
+			
+        	foreach( $data as $key => $val ){
+        		
+				/* ONLY ADD THOSE KEYS THAT ARE PART OF THE SHORTCODE */
+				if( array_key_exists( $key, $default_atts ) ){
+				
+					$val = str_replace("[","&#91;",$val);
+					$val = str_replace("]","&#93;",$val);
+        			
+					$shortcode .= $key.'="'.$val.'" ';				/* SHORTCODE STRING ADD ATTRIBUTES */
+				}
+        	}
+        	$shortcode .= ']';										/* SHORTCODE STRING END */
+        		
+			return $shortcode;
+		}
+		
 	}
--- code/wp-content/themes/Akvo-responsive/so-widgets/akvo-nested-filters/templates/template.php
+++ code/wp-content/themes/Akvo-responsive/so-widgets/akvo-nested-filters/templates/template.php
@@ -1,11 +1,13 @@
 <?php
 	
-	echo do_shortcode('[akvo_nested_filters title="'.$instance['title'].'" showposts="'.$instance['showposts'].'" post_type="'.$instance['post_type'].'" primary_filter="'.$instance['primary_filter'].'" secondary_filter="'.$instance['secondary_filter'].'"]');
+	global $akvo_shortcode_nested_filters;
+	
+	echo do_shortcode( $akvo_shortcode_nested_filters->form_shortcode( $instance ) );
 	
 	/*
-		
-		Find the shortcode [akvo_nested_filters] in inc/shortcodes.php
-	
+	*	
+	*	Find the shortcode [akvo_nested_filters] in inc/shortcodes/
+	*
 	*/
 	
 ?>
