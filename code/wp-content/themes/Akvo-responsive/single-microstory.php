a0d18147386a432b4c35aad35940638a76123415
Samuel Thomas <samvthom16@gmail.com>
2018-08-03 09:07:11 +0530

    [#1299] - single template for microstory

---
+++ code/wp-content/themes/Akvo-responsive/single-microstory.php
@@ -0,0 +1,19 @@
+<?php
+/*
+	Single MicroStory
+*/
+?>
+<?php get_header(); ?>
+
+<div id="content" class="floats-in micro-story">
+	
+    <?php if ( have_posts() ): while ( have_posts() ) : the_post(); ?>
+   
+    <?php the_content(); ?>
+    <?php endwhile; // end of the loop. ?>
+  
+	<?php endif;?>
+	<?php the_hubs_list();?>
+</div>
+<br style="clear:both;">
+<?php get_footer(); ?>
